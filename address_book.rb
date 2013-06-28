4379b1d275905b830d8c058bb44a94bdbd4533c0
Jamal Powell <jamalkpowell@gmail.com>
2013-06-27 22:17:39 -0400

    Added a form

--- address_book.rb
+++ address_book.rb
@@ -24,30 +24,36 @@ puts "Aight cool cool, You're lookin good, throw me yo numbah"
 
 puts "PS.....The back of yo head is ridiculous!"
 
+gets '/form'
 
-# This establishes a connection to the databse
-db = PG.connect(:dbname => 'address_book',
-                :host => 'localhost')
-# executing SQL code.
-# Passing a string of sql to the Database
-# db.exec( "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('Michael', 'Jackson', 50, 'M', False, '111-111-1111'); ") do |result|
-#   result.each do |row|
-#     binding.pry
-#   end
-# end
-
-sql =  "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('#{@first}', '#{@last}', #{@age}, '#{@gender}', #{@dtgd}, '#{@phone}')"
-db.exec(sql)
-
-db.close
-
-db = PG.connect(:dbname => 'address_book',
-                :host => 'localhost')
-show =  db.exec("select * from contacts")
- show.each do |row|
-  puts row
+erb :form
 end
-db.close
 
+gets '/'
+# This establishes a connection to the databse
+  db = PG.connect(:dbname => 'address_book',
+                  :host => 'localhost')
+  # executing SQL code.
+  # Passing a string of sql to the Database
+  # db.exec( "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('Michael', 'Jackson', 50, 'M', False, '111-111-1111'); ") do |result|
+  #   result.each do |row|
+  #     binding.pry
+  #   end
+  # end
 
+  sql =  "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('#{@first}', '#{@last}', #{@age}, '#{@gender}', #{@dtgd}, '#{@phone}')"
+  db.exec(sql)
+
+  db.close
+
+  db = PG.connect(:dbname => 'address_book',
+                  :host => 'localhost')
+  show =  db.exec("select * from contacts")
+   show.each do |row|
+    puts row
+  end
+  db.close
+
+  erb :form
+end
 
