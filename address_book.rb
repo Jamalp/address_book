b74ad2c1c96e15b435cfe2a5a42e725226a62a14
Jamal Powell <jamalkpowell@gmail.com>
2013-06-27 22:34:15 -0400

    added a new erb file..

--- address_book.rb
+++ address_book.rb
@@ -4,35 +4,48 @@ require 'sinatra'
 require 'sinatra-reloader'
 
 
-puts  "hey girl, what's yo first name??"
-@first = gets.chomp
+# puts  "hey girl, what's yo first name??"
+# @first = gets.chomp
 
-puts  "hey girl, what's yo last name??"
-@last = gets.chomp
+# puts  "hey girl, what's yo last name??"
+# @last = gets.chomp
 
-puts "hey girl hey, give me your age. Ain't lookin for no Jail Bait"
-@age = gets.chomp
+# puts "hey girl hey, give me your age. Ain't lookin for no Jail Bait"
+# @age = gets.chomp
 
-puts "hey girl hey..hey. No judgement, but whats your gender?"
-@gender = gets.chomp
+# puts "hey girl hey..hey. No judgement, but whats your gender?"
+# @gender = gets.chomp
 
-puts "Yo, I'm really lookin for someone who can get down, is that you? Tell me thats true and not false."
-@dtgd = gets.chomp
+# puts "Yo, I'm really lookin for someone who can get down, is that you? Tell me thats true and not false."
+# @dtgd = gets.chomp
 
-puts "Aight cool cool, You're lookin good, throw me yo numbah"
-@phone = gets.chomp
+# puts "Aight cool cool, You're lookin good, throw me yo numbah"
+# @phone = gets.chomp
 
-puts "PS.....The back of yo head is ridiculous!"
+# puts "PS.....The back of yo head is ridiculous!"
 
-gets '/form'
+get '/form' do
+
 
 erb :form
 end
 
-gets '/'
+get '/'
 # This establishes a connection to the databse
+
+  person = []
   db = PG.connect(:dbname => 'address_book',
                   :host => 'localhost')
+ db.exec("select * from contacts") do |result|
+      result.each do |row|
+        @person << row
+      end
+    end
+  db.close
+  erb :new_person
+
+
+
   # executing SQL code.
   # Passing a string of sql to the Database
   # db.exec( "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('Michael', 'Jackson', 50, 'M', False, '111-111-1111'); ") do |result|
@@ -41,19 +54,19 @@ gets '/'
   #   end
   # end
 
-  sql =  "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('#{@first}', '#{@last}', #{@age}, '#{@gender}', #{@dtgd}, '#{@phone}')"
-  db.exec(sql)
+#   sql =  "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('#{@first}', '#{@last}', #{@age}, '#{@gender}', #{@dtgd}, '#{@phone}')"
+#   db.exec(sql)
 
-  db.close
+#   db.close
 
-  db = PG.connect(:dbname => 'address_book',
-                  :host => 'localhost')
-  show =  db.exec("select * from contacts")
-   show.each do |row|
-    puts row
-  end
-  db.close
+#   db = PG.connect(:dbname => 'address_book',
+#                   :host => 'localhost')
+#   show =  db.exec("select * from contacts")
+#    show.each do |row|
+#     puts row
+#   end
+#   db.close
 
-  erb :form
-end
+#   erb :form
+# end
 
