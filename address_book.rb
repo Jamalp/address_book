93237de1c450154004c52c186b79e0f51f410998
Jamal Powell <jamalkpowell@gmail.com>
2013-06-27 21:56:32 -0400

    added a story/questions

--- address_book.rb
+++ address_book.rb
@@ -1,46 +1,49 @@
-# use this code to create an address book in sinatra
-# create an input form
-# add a person
-# list people
 require 'pg'
 require 'pry'
+require 'sinatra'
+require 'sinatra-reloader'
 
-puts "Hey gurl"
 
-# get all the inputs
-# put them in the string
-# make it work
+puts  "hey girl, what's yo first name??"
+@first = gets.chomp
 
-# this establishes a connection to the database
-# db = PG.connect(:dbname => 'address_book',
-#   :host => 'localhost')
-# executing sql code
-# passing a string of sql to the database
+puts  "hey girl, what's yo last name??"
+@last = gets.chomp
 
-# insert into database
+puts "hey girl hey, give me your age. Ain't lookin for no Jail Bait"
+@age = gets.chomp
+
+puts "hey girl hey..hey. No judgement, but whats your gender?"
+@gender = gets.chomp
+
+puts "Yo, I'm really lookin for someone who can get down, is that you? Tell me thats true and not false."
+@dtgd = gets.chomp
+
+puts "Aight cool cool, You're lookin good, throw me yo numbah"
+@phone = gets.chomp
+
+puts "PS.....The back of yo head is ridiculous!"
+
+
+# This establishes a connection to the databse
 db = PG.connect(:dbname => 'address_book',
-  :host => 'localhost')
-
-puts "what's your name girl?"
-name = gets.chomp
-sql = "insert into contacts (first) values ('#{name}')"
-db.exec(sql)
-sql = "select first, age from contacts"
-db.exec(sql) do |result|
-  result.each do |row|
-    puts row
-  end
-end
-# db.close
-db.close
-
-# reads from database
-# db = PG.connect(:dbname => 'address_book',
-#   :host => 'localhost')
-# sql = "select first, age from contacts"
-# db.exec(sql) do |result|
+                :host => 'localhost')
+# executing SQL code.
+# Passing a string of sql to the Database
+# db.exec( "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('Michael', 'Jackson', 50, 'M', False, '111-111-1111'); ") do |result|
 #   result.each do |row|
-#     puts row
+#     binding.pry
 #   end
 # end
-# db.close
+
+sql =  "insert into contacts(first, last, age, gender, dtgd, phone) VALUES ('#{@first}', '#{@last}', #{@age}, '#{@gender}', #{@dtgd}, '#{@phone}')"
+db.exec(sql)
+
+show =  db.exec("select * from contacts")
+ show.each do |row|
+  puts row
+end
+db.close
+
+
+
